3b1cd37ee44cfd5be19b771ee84351c92945f150
fahamfhm <fahamfahim2003@gmail.com>
2025-05-08 19:23:05 +0530

    vercel

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,10 +1,25 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
+// Create connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'store'
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT,
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
+});
+
+// Connect and handle errors
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database');
 });
 
 module.exports = db;
