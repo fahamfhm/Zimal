31e0e2f17982b7536281b714bc205e5d80128a48
fahamfhm <fahamfahim2003@gmail.com>
2025-05-08 21:00:46 +0530

    index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const foodRoutes = require('./routes/foods'); // food API
-const cosmeticsRoutes = require('./routes/cosmetics');
+const foodRoutes = require('./routes/foods.js'); // food API
+const cosmeticsRoutes = require('./routes/cosmetics.js');
 const spicesRoutes = require('./routes/spices.js');
 
 
